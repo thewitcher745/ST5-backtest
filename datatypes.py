783a59b8a3f751ca8286cddaa08f2cc1408e8c11
thewitcher745 <thewitcher745@gmail.com>
2024-04-16 18:52:13 +0330

    FEAT: Added highlight range option to plotting function Added LegTupleType definition

--- datatypes.py
+++ datatypes.py
@@ -2,7 +2,6 @@ from typing import NamedTuple, List, Tuple, Type, Union
 import pandas as pd
 from datetime import datetime
 
-NamedTupleColumnType = List[Tuple[str, type]]
 
 class CandleTupleType(NamedTuple):
     pair_df_index: int
@@ -12,8 +11,22 @@ class CandleTupleType(NamedTuple):
     low: float
     close: float
 
-pivot_columns: NamedTupleColumnType = [('pair_df_index', int), ('time', datetime), ('pivot_value', float), ('pivot_type', str)]
-PivotTupleType: Type[NamedTuple] = NamedTuple('Pivot', pivot_columns)
+
+class PivotTupleType(NamedTuple):
+    pair_df_index: int
+    time: datetime
+    pivot_value: float
+    pivot_type: str
+
+
+class LegTupleType(NamedTuple):
+    start_index: int
+    end_index: int
+    start_time: datetime
+    end_time: datetime
+    start_value: float
+    end_value: float
+    leg_type: str
 
 
 def create_candle_tuple(row: Union[pd.Series, CandleTupleType]) -> CandleTupleType:
@@ -27,4 +40,11 @@ def create_pivot_tuple(pivot: tuple) -> PivotTupleType:
     pivot_candle: CandleTupleType = pivot[0]
     pivot_type: str = pivot[1]
     pivot_value: float = pivot_candle.high if pivot_type == "peak" else pivot_candle.low
+
     return PivotTupleType(pivot_candle.pair_df_index, pivot_candle.time, pivot_value, pivot_type)
+
+
+def create_leg_tuple(pivot_1: PivotTupleType, pivot_2: PivotTupleType) -> LegTupleType:
+    leg_type = "bullish" if pivot_1.pivot_value < pivot_2.pivot_value else "bearish"
+
+    return LegTupleType(pivot_1.pair_df_index, pivot_2.pair_df_index, pivot_1.time, pivot_2.time, pivot_1.pivot_value, pivot_2.pivot_value, leg_type)
--- general_utils.py
+++ general_utils.py
@@ -7,12 +7,12 @@ def load_local_data(hdf_path: str = './cached_data/XEMUSDT.hdf5') -> pd.DataFram
     return pd.DataFrame(pd.read_hdf(hdf_path))
 
 
-def plot_candlestick_with_peaks(df: pd.DataFrame, zigzag_df: pd.DataFrame=None, x_axis_type="time"):
+def plot_candlesticks_zigzag_range(df: pd.DataFrame, zigzag_df: pd.DataFrame = None, x_axis_type: str = "time", highlight_range=None):
     fig = go.Figure()
 
     if x_axis_type == "index":
         candlestick_x_data = df.index
-        zigzag_x_data = df.pair_df_index
+        zigzag_x_data = zigzag_df.pair_df_index
     else:
         candlestick_x_data = df.time
         zigzag_x_data = zigzag_df.time
@@ -31,6 +31,39 @@ def plot_candlestick_with_peaks(df: pd.DataFrame, zigzag_df: pd.DataFrame=None, 
                                  mode='lines+markers',
                                  name='Zigzag version 2',
                                  line=dict(color='royalblue')))
+
+    # Add a highlighted range if specified
+    if highlight_range is not None:
+        if type(highlight_range) == tuple:
+            fig.add_shape(
+                type="rect",
+                xref="x",
+                yref="paper",
+                x0=highlight_range[0],
+                y0=0,
+                x1=highlight_range[1],
+                y1=1,
+                fillcolor="LightSalmon",
+                opacity=0.5,
+                layer="below",
+                line_width=0,
+            )
+        elif type(highlight_range) == list:
+            color_list = ["LightSalmon", "LightGreen", "LightBlue", "LightCoral", "LightSkyBlue", "LightPink"]
+            for i, range_tuple in enumerate(highlight_range):
+                fig.add_shape(
+                    type="rect",
+                    xref="x",
+                    yref="paper",
+                    x0=range_tuple[0],
+                    y0=0,
+                    x1=range_tuple[1],
+                    y1=1,
+                    fillcolor=color_list[i],
+                    opacity=0.5,
+                    layer="below",
+                    line_width=0,
+                )
 
     fig.update_layout(title='Candlestick Chart with Peaks',
                       xaxis_title='Date',
