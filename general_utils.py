8c62384fa48d33cec5130ac8508a60ce71478c21
thewitcher745 <thewitcher745@gmail.com>
2024-04-24 17:07:49 +0330

    FEAT: PlottingTool class implemented, allows for incremental plotting of different elements

--- general_utils.py
+++ general_utils.py
@@ -8,6 +8,103 @@ def load_local_data(hdf_path: str = './cached_data/XEMUSDT.hdf5') -> pd.DataFram
     return pd.DataFrame(pd.read_hdf(hdf_path))
 
 
+class PlottingTool:
+    def __init__(self, x_axis_type='index'):
+        self.fig = go.Figure()
+        self.x_axis_type = x_axis_type
+        self.number_drawn_ranges = 0
+        self.range_color_list = ['LightSalmon',
+                                 'LightGreen',
+                                 'LightBlue',
+                                 'LightCoral',
+                                 'LightSkyBlue',
+                                 'LightPink',
+                                 'LightYellow',
+                                 'LightGray',
+                                 'LightCyan',
+                                 'LightGoldenrod',
+                                 'LightSeaGreen']
+
+    def draw_candlesticks(self, df) -> None:
+        # Set the candlestick data to be plotted from the time values instead of pair_df_indices if the x_axis_type is time
+        candlestick_x_data = df.index
+        if self.x_axis_type == 'time':
+            candlestick_x_data = df.time
+
+        self.fig.add_trace(go.Candlestick(x=candlestick_x_data,
+                                          open=df['open'],
+                                          high=df['high'],
+                                          low=df['low'],
+                                          close=df['close'],
+                                          name='Candlestick')
+                           )
+
+    def draw_zigzag(self, zigzag_df, title='Zigzag', color='royalblue') -> None:
+        # Set the zigzag data to be plotted from the time values instead of pair_df_indices if the x_axis_type is time
+
+        zigzag_x_data = zigzag_df.pair_df_index
+        if self.x_axis_type == 'time':
+            zigzag_x_data = zigzag_df.time
+
+        # Plot the zigzag with the entered or default parameters
+        self.fig.add_trace(go.Scatter(x=zigzag_x_data,
+                                      y=zigzag_df.pivot_value,
+                                      mode='lines+markers',
+                                      name=title,
+                                      line=dict(color=color)))
+
+    def draw_highlight(self, highlight_range) -> None:
+        # If a single highlight range has been given, draw it
+        if type(highlight_range) == tuple:
+            # Add one to the number of ranges drawn, this variable is used to keep colors separated
+            # If there are no more colors, loop back to the first one
+            self.number_drawn_ranges += 1
+            next_color = self.range_color_list[self.number_drawn_ranges % len(self.range_color_list) - 1]
+
+            self.fig.add_shape(
+                type='rect',
+                xref='x',
+                yref='paper',
+                x0=highlight_range[0],
+                y0=0,
+                x1=highlight_range[1],
+                y1=1,
+                fillcolor=next_color,
+                opacity=0.5,
+                layer='below',
+                line_width=0,
+            )
+        elif type(highlight_range) == list:
+            for i, range_tuple in enumerate(highlight_range):
+                self.number_drawn_ranges += 1
+                next_color = next_color = self.range_color_list[self.number_drawn_ranges % len(self.range_color_list) - 1]
+
+                self.fig.add_shape(
+                    type='rect',
+                    xref='x',
+                    yref='paper',
+                    x0=range_tuple.start_index,
+                    y0=0,
+                    x1=range_tuple.end_index,
+                    y1=1,
+                    fillcolor=next_color,
+                    opacity=0.5,
+                    layer='below',
+                    line_width=0,
+                )
+
+    def show(self, title: str = 'Price Chart',
+             xaxis_title: str = 'Date',
+             yaxis_title: str = 'Price'):
+
+        self.fig.update_layout(title=title,
+                               xaxis_title=xaxis_title,
+                               yaxis_title=yaxis_title,
+                               xaxis_rangeslider_visible=False)
+
+        self.fig.show()
+
+
 def plot_candlesticks_zigzag_range(df: pd.DataFrame,
                                    zigzag_df: pd.DataFrame = None,
                                    x_axis_type: str = 'time',
