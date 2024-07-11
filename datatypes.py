b70f326d018c647311474c3ad723a64a6d3db67d
thewitcher745 <thewitcher745@gmail.com>
2024-07-11 22:32:58 +0330

    FEAT: Implemented Box class for positions Boxes can currently form from any base candle and have their tops and bottoms on the highs and lows

--- datatypes.py
+++ datatypes.py
@@ -18,6 +18,7 @@ class Candle(NamedTuple):
         else:
             return Candle(row.Index, row.time, row.open, row.high, row.low, row.close)
 
+
 class Pivot(NamedTuple):
     pair_df_index: int
     time: datetime
@@ -51,7 +52,7 @@ class Leg(NamedTuple):
         leg_type = 'bullish' if pivot_1.pivot_value < pivot_2.pivot_value else 'bearish'
 
         return Leg(pivot_1.pair_df_index, pivot_2.pair_df_index, pivot_1.time, pivot_2.time, pivot_1.pivot_value, pivot_2.pivot_value,
-                            leg_type)
+                   leg_type)
 
 
 class OneDChain(NamedTuple):
@@ -64,3 +65,64 @@ class OneDChain(NamedTuple):
     @staticmethod
     def create(low_chain_length: int, high_chain_length: int, start_pair_df_index: int, direction: str, is_forming_pbos: bool):
         return OneDChain(low_chain_length, high_chain_length, start_pair_df_index, direction, is_forming_pbos)
+
+
+class Box:
+    def __init__(self, base_candle: Candle, box_type: str):
+        self.start_index = base_candle.pair_df_index
+        self.base_candle = base_candle
+        self.type = box_type
+
+        self.top = base_candle.high
+        self.bottom = base_candle.low
+
+        self.is_valid = True
+        self.price_exit_index = None
+        self.price_reentry_indices = []
+
+    def check_box_entries(self, pair_df: pd.DataFrame) -> None:
+        """
+        Method to check the entries of the box and determine its validity.
+
+        This method checks when the price candlesticks in pair_df "exit" the box and whether they re-enter the box.
+        If there is a re-entry, the box is marked as invalid. All the indices are also registered.
+
+        Parameters:
+        pair_df (pd.DataFrame): The DataFrame containing the price data.
+
+        Returns:
+        None
+        """
+
+        # Get the subset of pair_df that we need to check
+        check_window = pair_df.iloc[self.start_index + 1:]
+
+        # If the box is of type "long"
+        if self.type == "long":
+            # Find the first index where the price exits the box
+            exit_index = check_window[check_window['low'] > self.top].first_valid_index()
+            if exit_index is not None:
+                self.price_exit_index = exit_index
+                # If an exit is found, check for a reentry into the box after the exit
+                # Should use check_window.loc[exit_index:] instead of iloc because the current df is a subset of pair_df, and the indices are
+                # all messed up
+                reentry_check_window = check_window.loc[exit_index:]
+                reentry_index = reentry_check_window.loc[reentry_check_window['low'] < self.top].first_valid_index()
+                # If a reentry is found, mark the box as invalid
+                if reentry_index is not None:
+                    self.price_reentry_indices.append(reentry_index)
+                    self.is_valid = False
+        else:  # If the box is of type "short"
+            # Find the first index where the price exits the box
+            exit_index = check_window[check_window['high'] < self.bottom].first_valid_index()
+            if exit_index is not None:
+                self.price_exit_index = exit_index
+                # If an exit is found, check for a reentry into the box after the exit
+                # Should use check_window.loc[exit_index:] instead of iloc because the current df is a subset of pair_df, and the indices are
+                # all messed up
+                reentry_check_window = check_window.loc[exit_index:]
+                reentry_index = reentry_check_window.loc[reentry_check_window['high'] > self.bottom].first_valid_index()
+                # If a reentry is found, mark the box as invalid
+                if reentry_index is not None:
+                    self.price_reentry_indices.append(reentry_index)
+                    self.is_valid = False
