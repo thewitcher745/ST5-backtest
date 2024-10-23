4004046060d98a70f0e5de0b7d27fd98bc9a884a
thewitcher745 <thewitcher745@gmail.com>
2024-10-24 02:06:35 +0330

    FEAT: Added some more setups

--- position_prices_setup.py
+++ position_prices_setup.py
@@ -35,10 +35,48 @@ def all_on_5(position):
     if position.type == "long":
         position.stoploss = position.entry_price - constants.stoploss_coeff * position.edicl
         position.target_list = [
-            position.entry_price + 7 * position.edicl,
+            position.entry_price + 5 * position.edicl,
         ]
     else:
         position.stoploss = position.entry_price + constants.stoploss_coeff * position.edicl
         position.target_list = [
-            position.entry_price - 7 * position.edicl,
+            position.entry_price - 5 * position.edicl,
+        ]
+
+
+def all_on_3(position):
+    if position.type == "long":
+        position.stoploss = position.entry_price - constants.stoploss_coeff * position.edicl
+        position.target_list = [
+            position.entry_price + 3 * position.edicl,
+        ]
+    else:
+        position.stoploss = position.entry_price + constants.stoploss_coeff * position.edicl
+        position.target_list = [
+            position.entry_price - 3 * position.edicl,
+        ]
+
+
+def all_on_30(position):
+    if position.type == "long":
+        position.stoploss = position.entry_price - constants.stoploss_coeff * position.edicl
+        position.target_list = [
+            position.entry_price + 30 * position.edicl,
+        ]
+    else:
+        position.stoploss = position.entry_price + constants.stoploss_coeff * position.edicl
+        position.target_list = [
+            position.entry_price - 30 * position.edicl,
+        ]
+
+def all_on_60(position):
+    if position.type == "long":
+        position.stoploss = position.entry_price - constants.stoploss_coeff * position.edicl
+        position.target_list = [
+            position.entry_price + 60 * position.edicl,
+        ]
+    else:
+        position.stoploss = position.entry_price + constants.stoploss_coeff * position.edicl
+        position.target_list = [
+            position.entry_price - 60 * position.edicl,
         ]
